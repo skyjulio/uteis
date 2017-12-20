0b0174b777adbcde8421696c59289bed4504d640
skyjulio <skyjulio@gmail.com>
2017-12-20 21:03:39 -0200

    add text

--- pyton install.sql
+++ pyton install.sql
@@ -6,3 +6,31 @@ pip3 install jupyter
 jupyter-notebook
 
 http://localhost:8888/tree
+
+
+pip install virtualenv 
+biblioteca virtual (clone da instalação do python em determinada pasta)
+
+separando por versões 
+
+comandos servem apenas para esta pasta 
+
+
+virtualenv venv
+
+venv
+
+venc cd scrpits
+scrpits>activate
+
+pip install flask
+
+python import flask
+
+
+
+
+
+
+
+
