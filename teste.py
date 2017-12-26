752c27c4b52db984c2759c79d909fd1fe19c66bd
Julio <skyjulio@gmail.com>
2017-12-26 20:48:03 -0200

    save file

---
+++ teste.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+oi
+
+
+# In[4]:
+
+
+from IPython.display import Image
+from IPython.core.display import HTML
+Image(url="C:\temp2\a.jpg")
+
+
+# In[5]:
+
+
+print('Oi')
+
+
+# In[7]:
+
+
+4 * "Hello "
+
